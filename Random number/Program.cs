e7bbf6a923fb3846451da04fbdb2dc2d5465f8c9
Serdar Bozkurt <r0960532@uccl.be>
2025-02-05 17:15:56 +0100

    small changes

--- Random number/Program.cs
+++ Random number/Program.cs
@@ -4,7 +4,6 @@
     {
         static void Main(string[] args)
         {
-            // Create an instance of the Random class
             Random random = new Random();
             Console.Write("Geef een getal in: ");
             int firstNumber = int.Parse(Console.ReadLine());
@@ -12,7 +11,6 @@
             Console.Write("Geef een 2de getal in: ");
             int secondNumber = int.Parse(Console.ReadLine());
 
-            // Generate a random integer between 0 (inclusive) and 100 (exclusive)
             int randomNumber = random.Next(firstNumber, secondNumber+1);
 
             Console.WriteLine("Random number: " + randomNumber);
